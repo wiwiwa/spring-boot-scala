2291a8e10f39cad6d4595c886c240277c16f9b86
Samuel Shan <shm.shan@gmail.com>
2022-11-01 15:01:00 +0800

    applicationVersion: support tag like 'v1.12.3'

--- millSpringBoot/src/com/wiwiwa/mill/ScalaAppModule.scala
+++ millSpringBoot/src/com/wiwiwa/mill/ScalaAppModule.scala
@@ -26,22 +26,21 @@ trait JavaAppModule extends JavaModule {
   }
   def applicationVersion = T.input {
     implicit val pwd: os.Path = millSourcePath / os.up
-    val GitLog = """(?s)(.*?)\btag:\s+([^.]+)\.(\d*)([^,)]*).*""".r
-    os.proc("git","log","--decorate").call().out.text match {
-      case GitLog(prefix, vLeft, vMinor, vRight) =>
-        val isClean = os.proc("git","status").call().out.lines.last.endsWith(" clean")
-        val isHead = !prefix.contains("\n")
-        val version = {
-          val minor = if(isHead && isClean) vMinor.toInt else vMinor.toInt+1
-          s"$vLeft.${minor}$vRight"
-        }
-        if(isClean && !isHead) {
-          println(s"Adding new git tag: $version")
-          os.proc("git","tag", version).call()
-        }
-        if(isClean) version else "DEV-SNAPSHOT"
-      case _ => throw new IllegalStateException("A git tag not found in format for xxx.yyy")
-    }
+    val GitLog = """(.*)\.(\d+)(.*)""".r
+    val isClean = os.proc("git","status").call().out.lines.last.endsWith(" clean")
+    if(!isClean) "DEV-SNAPSHOT" else
+      os.proc("git","tag","--sort=-authordate").call().out.lines.head match {
+        case GitLog(vLeft, vMinor, vRight) =>
+          val isHead = os.proc("git", "show").call().out.lines.head.contains("HEAD")
+          val minor = if (isHead) vMinor.toInt else vMinor.toInt + 1
+          val version = s"$vLeft.$minor$vRight"
+          if (!isHead) {
+            println(s"Adding new git tag: $version")
+            os.proc("git", "tag", version).call()
+          }
+          version
+        case _ => throw new IllegalStateException("Latest tag is not in format for xxx.yyy")
+      }
   }
   override def manifest = T {
     super.manifest().add(
