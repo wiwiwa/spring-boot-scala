29dbc6432307066a364e71675c387ca9ecbc4afd
Samuel Shan <samuel.shan@me.com>
2023-06-12 08:15:19 +0800

    MillSpringBoot: upgrade `mill` to version `0.11.0`

--- build.sc
+++ build.sc
@@ -1,4 +1,4 @@
-import $ivy.`com.wiwiwa::mill-spring-boot:1.20`, com.wiwiwa.mill.ScalaLibraryModule
+import $ivy.`com.wiwiwa::mill-spring-boot:1.30`, com.wiwiwa.mill.ScalaLibraryModule
 import mill._
 import mill.scalalib._
 
@@ -21,10 +21,12 @@ object millSpringBoot extends ScalaLibraryModule {
     ivy"com.lihaoyi::mill-scalalib:$millVersion",
   )
 
-  object test extends Tests with TestModule.Utest {
+  object test extends ScalaTests with TestModule.Utest {
     override def ivyDeps = Agg(
       ivy"com.lihaoyi::utest:$uTestVersion",
       ivy"com.lihaoyi::mill-scalalib:$millVersion",
+      ivy"com.lihaoyi::mill-main-testkit:$millVersion",
+      ivy"com.lihaoyi::mill-main-api:$millVersion",
       ivy"org.springframework.boot:spring-boot:$springBootVersion"
         .excludeName("log4j-to-slf4j"), //log4j-to-slf4j conflicts with mill-scalalib-worker
     )
@@ -42,7 +44,7 @@ object springBootScala extends ScalaLibraryModule {
     ivy"org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion",
   )
 
-  object test extends Tests with TestModule.Utest {
+  object test extends ScalaTests with TestModule.Utest {
     override def ivyDeps = T{ springBootScala.compileIvyDeps() ++ Seq(
       ivy"com.lihaoyi::utest:$uTestVersion",
       ivy"com.h2database:h2:2.1.212",
--- millSpringBoot/test/src/com/wiwiwa/mill/MillSpringBootTest.scala
+++ millSpringBoot/test/src/com/wiwiwa/mill/MillSpringBootTest.scala
@@ -1,91 +1,53 @@
 package com.wiwiwa.mill
 
-import ammonite.main.Defaults
-import ammonite.util.Colors
 import com.wiwiwa.springboot.SpringBootScalaModule
-import mill.api.Result.{Exception, Failure, Success}
+import mill.api.Result
 import mill.define.Segment.Label
-import mill.define._
-import mill.eval.{Evaluator, EvaluatorPaths}
-import mill.moduledefs.Cacher
+import mill.define.{Segments, Target}
+import mill.main.MainModule
 import mill.scalalib.DepSyntax
-import mill.util.PrintLogger
+import mill.testkit.MillTestKit
 import mill.{Agg, T}
 import org.springframework.boot.loader.tools.Repackager
-import sourcecode.Name
+import sourcecode.{Enclosing, Line}
 import utest._
 
-object MillSpringBootTest extends TestSuite with Cacher {
-  val testApp = createModule()
+object MillSpringBootTest extends TestSuite with MillTestKit {
+  override def tests = Tests {
+    val retVal = runTask(springBootScalaTestModule.applicationVersion)
+    assert(retVal != "")
+  }
+
   val evaluator = createEvaluator()
-
-  override def tests = Tests{
-    val ret = testApp.assembly.value
-    assert( ret != null )
-  }
-
-  def createModule() = {
-    val name = "springBootScalaTest"
-    val projectPath = os.pwd/"springBootScala"/"test"
-    implicit val ctx = Ctx.make(
-      implicitly, implicitly, Name(name), BasePath(projectPath), Segments(),
-      Ctx.External(false), Ctx.Foreign(None), sourcecode.File(""), Caller("")
-    )
-    new TestSpringBootModule with SpringBootScalaModule {
-      override def scalaVersion = "3.2.1"
-      override def ivyDeps = T {
-        val springBootVersion = classOf[Repackager].getPackage.getImplementationVersion
-        super.ivyDeps() ++ Seq(
-          ivy"com.wiwiwa::spring-boot-test:SNAPSHOT",
-          ivy"com.lihaoyi::utest:0.8.1",
-          ivy"org.springframework.boot:spring-boot-starter-web:$springBootVersion",
-          ivy"org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion",
-          ivy"com.h2database:h2:2.1.212",
-          ivy"javax.xml.bind:jaxb-api:2.3.1",
-        )
-      }
-      override def millSourcePath = projectPath
-      implicit override def millModuleSegments = Segments(Label(name))
+  object springBootScalaTestModule extends BaseModule()(Enclosing("test"), Line(0))
+    with SpringBootScalaModule with MainModule {
+    override def scalaVersion = T {"3.2.1"}
+    override def ivyDeps = T {
+      val springBootVersion = classOf[Repackager].getPackage.getImplementationVersion
+      super.ivyDeps() ++ Seq(
+        ivy"com.wiwiwa::spring-boot-test:SNAPSHOT",
+        ivy"com.lihaoyi::utest:0.8.1",
+        ivy"org.springframework.boot:spring-boot-starter-web:$springBootVersion",
+        ivy"org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion",
+        ivy"com.h2database:h2:2.1.212",
+        ivy"javax.xml.bind:jaxb-api:2.3.1",
+      )
     }
+    override implicit def millModuleSegments = Segments(Seq(Label("test")))
   }
+  override def getSrcPathBase() = os.pwd / "springBootScala"
 
-  def createEvaluator() = {
-    val colors = Colors.Default
-    val logger = PrintLogger(
-      true, false,
-      colors.info(), colors.error(),
-      System.out, System.out, System.err, inStream = System.in,
-      false,
-      context = ""
-    )
-    val outDir = testApp.millSourcePath/"out"
-    Evaluator( Defaults.ammoniteHome, outDir, outDir, testApp, logger )
+  def createEvaluator() = new TestEvaluator(springBootScalaTestModule,
+      s"${os.pwd}/springBootScala/test".split('/').drop(1),
+      true, Some(1),
+      System.out, System.in, false,
+      Seq.empty
+    ).evaluator
+  def runTask[T](task:Target[T]) = {
+    val res = evaluator.evaluate(Agg(task))
+    res.failing.values.headOption
+      .map(_.head)
+      .collectFirst { case Result.Exception(e, _) => throw e }
+    res.rawValues.head.asSuccess.get.value.value
   }
-
-  implicit class MillTask(task:Task[_]) {
-    def value = {
-      clearCache()
-      evaluator.evaluate(Agg(task))
-        .results
-        .collect {
-          case (_, Success(v)) => v
-          case (t, Exception(e, _)) => throw new RuntimeException(s"$t failed", e)
-          case (t, Failure(msg, _)) => throw new RuntimeException(s"$t failed: $msg")
-        }.last
-    }
-    def clearCache() = {
-      val (map, _) = Evaluator.plan(Agg(task))
-      val Right(label) = map.keys().toSeq.last
-      val meta = EvaluatorPaths.resolveDestPaths(evaluator.outPath, evaluator.destSegments(label)).meta
-      os.remove(meta)
-    }
-  }
-}
-
-class RootModule extends ExternalModule {
-  override def millDiscover = Discover[this.type]
-}
-abstract class TestSpringBootModule extends RootModule with ScalaLibraryModule {
-  object test extends Tests
-  override def organization = "org"
 }
